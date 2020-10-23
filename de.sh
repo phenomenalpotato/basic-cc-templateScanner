dbc930e063b9ca559b1102a0bd068c6671613db0
Le <suportejump@gmail.com>
2020-10-23 14:38:07 -0300

    UPD basic-cc-workflow estruct

--- de.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-mkdir $HOME/secrets
-# --lote para evitar o comando interativo
-# --sim para supor "sim" para as perguntas
-gpg --quiet --batch --yes --decrypt --passphrase="$CC_KEY" \
---output $HOME/secrets/api.txt api.txt.gpg
